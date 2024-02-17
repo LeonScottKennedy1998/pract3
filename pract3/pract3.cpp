42acbedc6e962084bdd7755cac0b08168939567e
LeonScottKennedy1998 <karina_alekseeva_2006@mail.ru>
2024-02-17 09:34:18 +0300

    Скибиди доб доб доб доб ес ес ес

---
+++ pract3/pract3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "Rus");
+    int strok, stolb;
+    cout << "Введите количество строк: ";
+    cin >> strok;
+    cout << "Введите количество столбцов: ";
+    cin >> stolb;
+    double sum = 0;
+    double counta = 0;
+    double** doublearr = new double* [strok];
+    double average = 0;
+    for (int i = 0; i < strok; ++i)
+    {
+        doublearr[i] = new double[stolb];
+        for (int j = 0; j < stolb; ++j)
+        {
+            cin >> doublearr[i][j];
+            sum += doublearr[i][j];
+            counta++;
+        }
+    }
+    average = sum/counta;
+    cout << "Среднее значение = " << average;
+    system("pause");
+    return 0;
+}
